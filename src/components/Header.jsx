bc50faa6bb2986226ecff766b832fb6e9beb0787
Khodja <138789644+Abdurahmon-2004@users.noreply.github.com>
2024-05-01 18:04:42 +0500

    norm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,7 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-<<<<<<< HEAD
-    <header className="w-full min-h-screen bg-gray-100 dark:bg-gray-900">
-=======
     <header className="w-full bg-gray-100 dark:bg-gray-800">
->>>>>>> 0267fa547210e0a82b0ea1eb36b595ae358bcd4d
       
      <Navbar/>
     </header>
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 import { Outlet } from "react-router";
 import Sidebar from "./Sidebar";
 const Admin = () => {
   return (
-    <div>
+    <div>  
       <Sidebar />
       <Outlet />
-=======
-import { MuiTable } from "./anjumanlar/Tabel";
-
-import React from 'react'
-
-const Admin = () => {
-  return (
-    <div>
-      <MuiTable/>
->>>>>>> 0267fa547210e0a82b0ea1eb36b595ae358bcd4d
-    </div>
-  )
+     </div>
+);
 }
+
+
 
 export default Admin
 
