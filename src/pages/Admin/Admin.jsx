6eb2d9aeeed65c03aa6797f0eecd73b7e137957d
Ruslan <ruslantilovov2@gmail.com>
2024-05-01 11:58:20 +0300

    add admin plus

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,10 +1,11 @@
+import { MuiTable } from "./anjumanlar/Table";
 
 const Admin = () => {
   return (
     <div>
-      
+      <MuiTable />
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
