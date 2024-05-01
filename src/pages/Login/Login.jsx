4487eb42d25e5f6c039901bb675ebf6a3bab2903
saidmurod555 <bobo.sher314@gmail.com>
2024-05-01 11:29:19 +0500

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
                 />
               </div>
             </div>
@@ -64,6 +64,7 @@ const Login = () => {
               </div>
               <div className="mt-2">
                 <input
+                
                  id="password"
                 name="password"
                 type="password"
@@ -71,7 +72,7 @@ const Login = () => {
                 required
                 value={password}
                 onChange={handlePasswordChange}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
