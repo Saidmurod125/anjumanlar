e6324f0337e2a432780b373ffcc8c95d3c782442
saidmurod555 <bobo.sher314@gmail.com>
2024-05-03 12:21:09 +0500

    as cer off

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -50,7 +50,6 @@ function Menu() {
   };
 
   return (
-<<<<<<< HEAD
     <div className="max-w-[1570px] h-[780px] w-full m-auto py-10 px-4 relative group">
       <h1 className="mb-6 text-xl font-medium text-center dark:text-white">
         SAYT TEST REJIMIDA ISHLAMOQDA{" "}
@@ -102,11 +101,6 @@ function Menu() {
         ))}
       </div>
     </div>
-=======
-  <>
-    menyu
-  </>
->>>>>>> c4ab4596ad5c6dc167d64a489560f5b95822dcd5
   );
 }
 
