a14c3769a9c4c8e99a0a05a928ed4f375f916687
saidmurod555 <bobo.sher314@gmail.com>
2024-05-03 12:13:28 +0500

    as of fox matris

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -37,9 +37,9 @@ if (isLoggedIn) {
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
+                {/* <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
                   Email address
-                </label>
+                </label> */}
                 <div className="mt-2">
                   <input
                     id="email"
